528d19475fe6ae8785e25f0320b53f1c60579da6
jb-git-code <jayanshub18@gmail.com>
2025-05-21 19:14:34 +0530

    Contest Label

---
+++ Contest label/contestsnip.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        while(t>0){
+
+
+
+            
+            t--;
+        }
+
+	}
+}
