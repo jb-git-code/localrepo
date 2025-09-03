24b90184452ec3205920862d38a9e9b58a5ca77b
Jayanshu Bhardwaj <jayanshub18@gmail.com>
2025-09-03 17:14:12 +0530

    template update

--- Contest label/contestsnip.java
+++ Contest label/contestsnip.java
@@ -1,20 +1,37 @@
 import java.util.*;
-import java.lang.*;
 import java.io.*;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        while(t>0){
+public class Compare_and_Connect {
+    public static void main(String[] args) throws IOException {
+        FastScanner fs = new FastScanner();
+        StringBuilder output = new StringBuilder();
 
-
-
+        long t = fs.nextLong();
+        while (t-- > 0) {
             
-            t--;
+            }
         }
 
-	}
+        System.out.print(output.toString());
+    }
+
+    static class FastScanner {
+        private final BufferedReader br;
+        private StringTokenizer st;
+
+        FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+    }
 }
